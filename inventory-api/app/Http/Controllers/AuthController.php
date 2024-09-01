d42e50b64a7f0007e98a853bf50d59c20fe8faf9
JeftaMukti <115094745+JeftaMukti@users.noreply.github.com>
2024-09-01 22:35:11 +0700

    authentication with role done/check

---
+++ inventory-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed',
+            'role' => 'required|in:purchase,distribusi'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => Hash::make($fields['password']),
+            'role' => $fields['role'],
+        ]);
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => "Your Email Or Password Maybe Wrong"
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return [
+            'message' => 'you are logout'
+        ];
+    }
+}
--- inventory-api/routes/api.php
+++ inventory-api/routes/api.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Authentication Route
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');;
+//End Authentication Route
+
+//Role:Purchase Route
+Route::middleware(['auth:sanctum','role:purchase'])->group(function (){
+    Route::get('/purchase', function(){
+        return response()->json([
+            'message' => 'halo role purchase'
+        ]);
+    });
+});
+//End Role:Purchase Route
+
+//Role:Distribusi Route
+Route::middleware(['auth:sanctum','role:distribusi'])->group(function (){
+    Route::get('/distribusi', function(){
+        return response()->json([
+            'message' => 'halo role distribusi'
+        ]);
+    });
+});
+//End Role:Distribusi Route
